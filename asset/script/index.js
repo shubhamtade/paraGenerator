ccf4464c0e2b12b71f3e9d027f463e8aa5a1e7e5
Shubham Tade <shubham.tade@gmail.com>
2024-09-10 10:02:50 +0530

    Complete

--- asset/script/index.js
+++ asset/script/index.js
@@ -1,17 +1,39 @@
 const apiKey = "1fb3fbcb43fb42619d428661aa102718";
 const apiUrl =
-  "https://randommer.io/api/Text/LoremIpsum?loremType=business&type=words";
+  "https://randommer.io/api/Text/LoremIpsum?loremType=business&type=words&number=";
 
 let inputBox = document.getElementById("userInput");
 let outputBox = document.getElementById("output");
+let addResult = document.getElementById("container");
 
-function result() {
+async function result() {
   try {
     if (inputBox.value !== "") {
-      console.log(inputBox.value);
+      const URL = apiUrl + inputBox.value;
+      const response = await fetch(URL, {
+        headers: {
+          accept: "*/*",
+          "X-Api-Key": apiKey,
+        },
+      });
+
+      if (response.ok) {
+        const loremData = await response.json();
+
+        // console.log(loremData);
+
+        let item = document.createElement("p");
+        item.textContent = loremData;
+        item.id = "output";
+        addResult.appendChild(item);
+      } else {
+        throw new Error(`Response status: ${response.status}`);
+      }
+    } else {
+      throw new Error("Please enter number of word you want to generate.");
     }
   } catch (error) {
-    console.log("error");
+    alert(error.message);
   }
 }
 
